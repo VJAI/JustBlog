d947ad800d2890c93ea73ed78f6df199fcb47858
Vijaya Anand <vijay.prideparrot@gmail.com>
2013-03-05 12:23:41 +0530

    timezone issue fixed

---
+++ src/JustBlog/JustBlog/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace JustBlog
+{
+  public static class Extensions
+  {
+    public static string ToConfigLocalTime(this DateTime utcDT)
+    {
+      var istTZ = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
+      return String.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDT, istTZ).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbr"]);
+    }
+  }
+}
