0f22023d88a9a1b0a9e1d2af4ecd467d151717b7
Vijaya Anand <vijay.prideparrot@gmail.com>
2013-12-02 14:08:11 +0530

    un-commented error handling code

--- src/JustBlog/JustBlog/Global.asax.cs
+++ src/JustBlog/JustBlog/Global.asax.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Web;
 using JustBlog.Controllers;
 using JustBlog.Core;
@@ -40,56 +40,56 @@ namespace JustBlog
 
     protected void Application_Error(object sender, EventArgs e)
     {
-      //var httpContext = ((MvcApplication)sender).Context;
-      //var ex = Server.GetLastError();
-      //var status = ex is HttpException ? ((HttpException)ex).GetHttpCode() : 500;
+      var httpContext = ((MvcApplication)sender).Context;
+      var ex = Server.GetLastError();
+      var status = ex is HttpException ? ((HttpException)ex).GetHttpCode() : 500;
 
-      //// Is Ajax request? return json
-      //if (httpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
-      //{
-      //  httpContext.ClearError();
-      //  httpContext.Response.Clear();
-      //  httpContext.Response.StatusCode = status;
-      //  httpContext.Response.TrySkipIisCustomErrors = true;
-      //  httpContext.Response.ContentType = "application/json";
-      //  httpContext.Response.Write("{ success: false, message: \"Error occured in server.\" }");
-      //  httpContext.Response.End();
-      //}
-      //else
-      //{
-      //  var currentController = " ";
-      //  var currentAction = " ";
-      //  var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
+      // Is Ajax request? return json
+      if (httpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+      {
+        httpContext.ClearError();
+        httpContext.Response.Clear();
+        httpContext.Response.StatusCode = status;
+        httpContext.Response.TrySkipIisCustomErrors = true;
+        httpContext.Response.ContentType = "application/json";
+        httpContext.Response.Write("{ success: false, message: \"Error occured in server.\" }");
+        httpContext.Response.End();
+      }
+      else
+      {
+        var currentController = " ";
+        var currentAction = " ";
+        var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
 
-      //  if (currentRouteData != null)
-      //  {
-      //    if (currentRouteData.Values["controller"] != null &&
-      //        !String.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
-      //    {
-      //      currentController = currentRouteData.Values["controller"].ToString();
-      //    }
+        if (currentRouteData != null)
+        {
+          if (currentRouteData.Values["controller"] != null &&
+              !String.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
+          {
+            currentController = currentRouteData.Values["controller"].ToString();
+          }
 
-      //    if (currentRouteData.Values["action"] != null &&
-      //        !String.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
-      //    {
-      //      currentAction = currentRouteData.Values["action"].ToString();
-      //    }
-      //  }
+          if (currentRouteData.Values["action"] != null &&
+              !String.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
+          {
+            currentAction = currentRouteData.Values["action"].ToString();
+          }
+        }
 
-      //  var controller = new ErrorController();
-      //  var routeData = new RouteData();
+        var controller = new ErrorController();
+        var routeData = new RouteData();
 
-      //  httpContext.ClearError();
-      //  httpContext.Response.Clear();
-      //  httpContext.Response.StatusCode = status;
-      //  httpContext.Response.TrySkipIisCustomErrors = true;
+        httpContext.ClearError();
+        httpContext.Response.Clear();
+        httpContext.Response.StatusCode = status;
+        httpContext.Response.TrySkipIisCustomErrors = true;
 
-      //  routeData.Values["controller"] = "Error";
-      //  routeData.Values["action"] = "Index";
+        routeData.Values["controller"] = "Error";
+        routeData.Values["action"] = "Index";
 
-      //  controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
-      //  ((IController)controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
-      //}
+        controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
+        ((IController)controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
+      }
     }
   }
 }
